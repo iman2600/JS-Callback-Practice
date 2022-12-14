4253c322ffe433c7d56d11d325a1ca153a60c27d
Isaac Sirstins <113957847+iman2600@users.noreply.github.com>
2022-12-13 22:27:58 -0700

    Finished activity

--- move.js
+++ move.js
@@ -1,12 +1,62 @@
 function move(element) {
-    element.style.position = 'fixed'
+  element.style.position = "fixed";
 
-    function moveToCoordinates(left, bottom) {
-        element.style.left = left + 'px'
-        element.style.bottom = bottom + 'px'
+  function moveToCoordinates(left, bottom) {
+    element.style.left = left + "px";
+    element.style.bottom = bottom + "px";
+  }
+
+function moveWithArrowKeys(left, bottom, callback){
+    let direction = null;
+    let x = 100;
+    let y = 250;
+
+    function moveCharacter() {
+      if (direction === "west") {
+        x = x - 1;
+      }
+      if (direction === "north") {
+        y = y + 1;
+      }
+      if (direction === "east") {
+        x = x + 1;
+      }
+      if (direction === "south") {
+        y = y - 1;
+      }
+      element.style.left = x + "px";
+      element.style.bottom = y + "px";
     }
 
-    return {
-        to: moveToCoordinates
-    }
+    document.addEventListener("keydown", function (e) {
+      if (e.repeat) return;
+
+      if (e.key === "ArrowLeft") {
+        direction = "west";
+      }
+      if (e.key === "ArrowUp") {
+        direction = "north";
+      }
+      if (e.key === "ArrowRight") {
+        direction = "east";
+      }
+      if (e.key === "ArrowDown") {
+        direction = "south";
+      }
+      callback(direction)
+    });
+
+    document.addEventListener("keyup", function (e) {
+      direction = null;
+      callback(direction);
+    });
+
+    setInterval(moveCharacter, 1);
+}
+
+
+  return {
+    to: moveToCoordinates,
+    withArrowKeys: moveWithArrowKeys,
+  };
 }
